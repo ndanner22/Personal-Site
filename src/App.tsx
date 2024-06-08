7e9d44b08a60af5d7e8cf23e674028cf87d41ed5
Neil Danner <ndanner22@gmail.com>
2024-06-08 14:13:15 +0100

    create the header component and link it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header.tsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
