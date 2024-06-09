8b0fca9b06305c78a82e2ad6a467578ddd8a2c0b
Neil Danner <ndanner22@gmail.com>
2024-06-09 16:59:37 +0100

    Create Nav component and link to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header.tsx";
+import Nav from "./Components/Nav.tsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Nav />
     </>
   );
 }
