c890c62cc438f25f7ed1a66beafebbab254247ce
Neil Danner <ndanner22@gmail.com>
2024-06-14 14:44:59 +0100

    Remove extra flex container that was redundant inside the Projects.tsx component

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,10 +4,8 @@ export default function Projects() {
   return (
     <div className="projects-container">
       <h2 className="title">Projects!</h2>
-      <div className="project-body">
-        <div className="project-description">
-          <ProjectTextAnimation />
-        </div>
+      <div className="project-description">
+        <ProjectTextAnimation />
       </div>
     </div>
   );
