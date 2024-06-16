7044053b7e6c43a5d66716c8c2149acccb9e4e67
Neil Danner <ndanner22@gmail.com>
2024-06-16 20:28:47 +0100

    Add github links to project data

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -4,6 +4,7 @@ export interface Project {
   description: string;
   image: string;
   alt: string;
+  github: string;
 }
 
 export const projects: Project[] = [
@@ -11,17 +12,19 @@ export const projects: Project[] = [
     project_id: "1",
     title: "StayFresh",
     description:
-      "StayFresh is a mobile app that lets the user take a photo of their shopping receipt to automatically add items to their digital cupboard. Keeping track of your grocery expirery dates has never been simplier.",
+      "StayFresh is a mobile app that uses AI to automatically scan your food items, give you reminders and recipe ideas to prevent food waste.",
     image: "../../public/images/stay-fresh.png",
     alt: "picture of cell phone app containing a grocery list",
+    github: "https://github.com/ndanner22/Stay-Fresh-FE",
   },
   {
     project_id: "2",
     title: "NC-News Frontend",
     description:
-      "The frontend for a news website built using React and JavaScript, and hosted on render.com.",
+      "The frontend for a news website built using React and JavaScript, hosted on render.com.",
     image: "../../public/images/place-holder.jpeg",
     alt: "picture of homepage of a news website",
+    github: "https://github.com/ndanner22/nc-news",
   },
   {
     project_id: "3",
@@ -30,5 +33,6 @@ export const projects: Project[] = [
       "The backend for a news website developed by creating a REST API using JavaScript and PostgreSQL, hosted on elephantsql.com.",
     image: "../../public/images/portrait.png",
     alt: "picture of JavaScript code",
+    github: "https://github.com/ndanner22/news-engine",
   },
 ];
