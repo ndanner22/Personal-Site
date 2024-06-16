d2617b386c71f47d5e13cdbdff0229abcf82f6d6
Neil Danner <ndanner22@gmail.com>
2024-06-16 15:06:34 +0100

    Create function to propery dislplay projects data on projects page

--- src/Components/Pages/ContactPage.tsx
+++ src/Components/Pages/ContactPage.tsx
@@ -2,6 +2,19 @@ export default function ContactPage() {
   return (
     <div className="contact-container">
       <h2 className="title">Contact Page</h2>
+      <form className="contact-form">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            // value={formData.name}
+            // onChange={handleChange}
+            required
+          />
+        </div>
+      </form>
     </div>
   );
 }
--- src/Components/Pages/ProjectsPage.tsx
+++ src/Components/Pages/ProjectsPage.tsx
@@ -1,7 +1,18 @@
-export default function ProjectsPage() {
+import React, { FC } from "react";
+import { projects, Project } from "../../data/projectData"; // Adjust the import path as needed
+
+const ProjectPage: FC = () => {
   return (
     <div className="project-page-container">
-      <h2 className="title">Project Page</h2>
+      {projects.map((project: Project) => (
+        <section key={project.project_id} className="project-page-project">
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+          {/* Add more project details here if necessary */}
+        </section>
+      ))}
     </div>
   );
-}
+};
+
+export default ProjectPage;
