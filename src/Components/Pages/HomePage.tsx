77bd4b43f36f18802de37d4c915afccd4bcad010
Neil Danner <ndanner22@gmail.com>
2024-06-17 10:18:39 +0100

    Remove components TextAnimation and ProjectTextAnimation from repo as they are no longer needed

--- src/Components/Pages/HomePage.tsx
+++ src/Components/Pages/HomePage.tsx
@@ -1,12 +1,12 @@
 import AboutMe from "../AboutMe";
-import Projects from "../HomeProjects";
+import HomeProjects from "../HomeProjects";
 import Contact from "../Contact";
 
 export default function HomePage() {
   return (
     <>
       <AboutMe />
-      <Projects />
+      <HomeProjects />
       <Contact />
     </>
   );
