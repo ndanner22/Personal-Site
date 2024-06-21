1702a930f575e19c33baa98d322e38c54a2d2afc
Neil Danner <ndanner22@gmail.com>
2024-06-21 11:53:17 +0100

    Update styling for project item in ProjectCarousel

--- src/Components/Pages/HomePage.tsx
+++ src/Components/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "../AboutMe";
 import HomeProjects from "../HomeProjects";
+import Footer from "../Footer";
 
 export default function HomePage() {
   return (
     <>
       <AboutMe />
       <HomeProjects />
+      <Footer />
     </>
   );
 }
