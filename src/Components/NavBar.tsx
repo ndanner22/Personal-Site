f351b3726e93c1074972ab142d20ac69739cafc8
Neil Danner <ndanner22@gmail.com>
2024-06-24 19:09:39 +0100

    Convert NavBar.tsx to TypeScript

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,20 +1,29 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
+const NavBar: React.FC = () => {
   return (
     <>
       <ul className="nav-bar">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="/Projects">
-          Projects
-        </Link>
-        <Link className="nav-link" to="/Contact">
-          Contact
-        </Link>
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/Projects">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/Contact">
+            Contact
+          </Link>
+        </li>
       </ul>
       <hr className="divider" />
     </>
   );
-}
+};
+
+export default NavBar;
