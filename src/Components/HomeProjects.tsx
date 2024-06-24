0a235578483ce66979669458e696d9aadb86ed61
Neil Danner <ndanner22@gmail.com>
2024-06-24 19:08:31 +0100

    Convert HomeProjects.tsx to TypeScript

--- src/Components/HomeProjects.tsx
+++ src/Components/HomeProjects.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import ProjectCarousel from "./ProjectCarousel";
 
-export default function Projects() {
+const Projects: React.FC = () => {
   return (
     <>
       <h2 className="title">Previous Projects</h2>
       <ProjectCarousel />
     </>
   );
-}
+};
+
+export default Projects;
